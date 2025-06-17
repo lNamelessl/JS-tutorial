acb9124df12c36559614f711519fff9b1696e9eb
lNameless <ikasima0@gmail.com>
2025-06-17 11:35:03 +0100

    upd:operators

---
+++ operators.js
@@ -0,0 +1,15 @@
+// + - * / ** %
+
+let students = 30;
+
+console.log(students + 1);
+console.log(students -1 );
+console.log(students * 1);
+console.log(students % 2);
+console.log(students ** 0);
+console.log(students % 4);
+students++;
+
+console.log(students -= 1);
+
+
