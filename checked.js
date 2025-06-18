673010ea92791896f7558d13f0db6a88364c4987
lNameless <ikasima0@gmail.com>
2025-06-18 10:26:27 +0100

    upd:checked in js

---
+++ checked.js
@@ -0,0 +1,13 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+
+
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+     subResult.textContent = `You have already subscribed!`   
+    }
+    else if(myCheckBox){
+        subResult.textContent = `You are not subscribed`
+    }
+}
