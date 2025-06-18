f957db1f102e965df2d4f522e8a6a91a24d7ea1e
lNameless <ikasima0@gmail.com>
2025-06-18 11:14:05 +0100

    upd:switch in js

---
+++ switch.js
@@ -0,0 +1,17 @@
+// switches
+
+let day = ``;
+
+switch(day){
+    case `monday`:
+    console.log('today is monday')
+    break
+    case `tuesday`:
+    console.log(`today is tuesday`)
+    break
+    default:
+        console.log(`today is friday`)
+        break
+}
+
+
