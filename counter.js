e3119dbaba303dc7ef75dd4d782219e60d8fa258
lNameless <ikasima0@gmail.com>
2025-06-18 09:23:32 +0100

    counter program

---
+++ counter.js
@@ -0,0 +1,20 @@
+// counter program
+
+const increaseBtn = document.getElementById("increaseBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const countLabel = document.getElementById("countLabel");
+
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    countLabel.textContent = 0;
+}
