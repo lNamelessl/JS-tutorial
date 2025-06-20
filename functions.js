132199e364cc8287cb615688d174c0475bb9ee0d
lNameless <ikasima0@gmail.com>
2025-06-20 09:50:58 +0100

    upd: callbacks

--- functions.js
+++ functions.js
@@ -1,8 +1,22 @@
 // functions
 let email = "fakefakemail.com"
+const myH1 = document.getElementById("myH1");
+
 
 function isValidEmail(email){
     return email.includes("@") ? true : false;
 }   
 
 console.log(isValidEmail(email)); 
+
+
+// callbacks
+function hello(){
+    console.log(`Hello`)
+}
+
+function goodbye(){
+    console.log(`Goodbye`)
+}
+
+hello(goodbye())
