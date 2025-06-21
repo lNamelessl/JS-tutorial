bc3c68c0113590630996b5f9b5367637988aae79
lNameless <ikasima0@gmail.com>
2025-06-21 09:15:50 +0100

    map method in js

--- functions.js
+++ functions.js
@@ -26,10 +26,20 @@ let fruits = ["apple", "banana", "mango"];
 fruits.forEach(capitalize);
 fruits.forEach(display);
 
+
 function capitalize(element, index, array){
     array[index] = element.charAt(0).toUpperCase() + element.slice(1)
 }
 
 function display(element){
     console.log(element)
+}
+
+const dates = ["2025-6-6", "2025-4-4", "2025-8-9"];
+formattedDates = dates.map(formatDates);
+console.log(formattedDates);
+
+function formatDates(element){
+    const parts = element.split("-");
+    return `${parts[1]}/${parts[2]}/${parts[0]}`
 }
