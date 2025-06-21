01e1b2eea5ecb7d430f8a0a99bfdf82757cbfd59
lNameless <ikasima0@gmail.com>
2025-06-21 09:23:57 +0100

    filters in js

--- functions.js
+++ functions.js
@@ -1,3 +1,12 @@
+// filter
+let numbers = [10, 11, 12, 17, 18, 20, 22]
+adultAges = numbers.filter(isAdult);
+console.log(adultAges);
+
+function isAdult(element){
+    return element >= 18 ;
+}
+
 // functions
 let email = "fakefakemail.com"
 const myH1 = document.getElementById("myH1");
@@ -35,11 +44,13 @@ function display(element){
     console.log(element)
 }
 
+// map
 const dates = ["2025-6-6", "2025-4-4", "2025-8-9"];
-formattedDates = dates.map(formatDates);
+let formattedDates = dates.map(formatDates);
 console.log(formattedDates);
 
 function formatDates(element){
     const parts = element.split("-");
     return `${parts[1]}/${parts[2]}/${parts[0]}`
 }
+
