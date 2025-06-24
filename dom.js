58c999b85bb26582991b2805b6636f317a70e979
lNameless <ikasima0@gmail.com>
2025-06-24 09:18:58 +0100

    dom in js

---
+++ dom.js
@@ -0,0 +1,16 @@
+const beets =  document.getElementById("beets");
+beets.style.backgroundColor = "yellow";
+
+const fruits = document.getElementsByClassName("fruits");
+Array.from(fruits).forEach(fruit => {fruit.style.backgroundColor = "red"});
+
+h4 = document.getElementsByTagName("h4");
+Array.from(h4).forEach(h => {h.style.backgroundColor = "blue"});
+
+const fruit = document.querySelector("li");
+fruit.style.backgroundColor = "lightgreen"
+
+const nRoots = document.querySelectorAll("ol");
+nRoots.forEach( nRoot =>{
+    nRoot.style.backgroundColor = "orange"
+} )
