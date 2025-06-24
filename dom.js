4270102ffdca38f35d8b1fb5c8097c7f1a94777e
lNameless <ikasima0@gmail.com>
2025-06-24 09:40:40 +0100

    dom nav injs

--- dom.js
+++ dom.js
@@ -1,16 +1,20 @@
-const beets =  document.getElementById("beets");
+const beets = document.getElementById("beets");
 beets.style.backgroundColor = "yellow";
 
 const fruits = document.getElementsByClassName("fruits");
-Array.from(fruits).forEach(fruit => {fruit.style.backgroundColor = "red"});
+Array.from(fruits).forEach(fruit => { fruit.style.backgroundColor = "red" });
 
 h4 = document.getElementsByTagName("h4");
-Array.from(h4).forEach(h => {h.style.backgroundColor = "blue"});
+Array.from(h4).forEach(h => { h.style.backgroundColor = "blue" });
 
 const fruit = document.querySelector("li");
 fruit.style.backgroundColor = "lightgreen"
 
 const nRoots = document.querySelectorAll("ol");
-nRoots.forEach( nRoot =>{
+nRoots.forEach(nRoot => {
     nRoot.style.backgroundColor = "orange"
-} )
+})
+
+
+const sibling = beets.previousElementSibling
+sibling.style.backgroundColor = "azure"
